2139ac995a3be4b4c1c1fd68048b9e2dd1b2acc0
nika <jamaspishvilinika@icloud.com>
2023-06-27 18:57:43 +0400

    started working on contact part

---
+++ client/Home_components/Contact.jsx
@@ -0,0 +1,19 @@
+import "../Home_css/Contact.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faInstagram,faFacebook,faTwitter} from "@fortawesome/free-brands-svg-icons"
+
+export default function Contact(){
+   return <div className="Contact_main_div">
+     <div className="Contact_info">
+        <h2>Email: jamaspishvilinika@icloud.com</h2>
+        <h2>Phone: 591-19-15-14</h2>
+     </div>
+     <div className="Contact_icons">
+   <div className="Icons_div">
+   <img src="https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c521.png" />
+   <img src="https://cdn.freebiesupply.com/logos/large/2x/facebook-3-logo-png-transparent.png"/>
+   <img src="https://cdn.freebiesupply.com/logos/large/2x/twitter-3-logo-png-transparent.png"/>
+    </div>
+     </div>
+    </div>
+}
--- client/big_components/Home.jsx
+++ client/big_components/Home.jsx
@@ -2,12 +2,14 @@ import '../Home_css/Home.css';
 import Header from '../Home_components/Header';
 import Body  from "../Home_components/Body";
 import Videos from '../Home_components/Videos';
+import Contact from '../Home_components/Contact';
 
 const Home=()=>{
   return <div className='Home_main_div'>
    <Header />
    <Body />
    <Videos/>
+   <Contact />
 </div>    
 }
 
